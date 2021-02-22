7e27880c720532effbae663fdae8cd1a0845cb34
Nami Falhan <namfaldev@gmail.com>
2021-02-22 17:29:26 +0100

    adjusted getHistoricalData function to return data tarting rom the midnight, and sorted the returned data

--- src/services/services.js
+++ src/services/services.js
@@ -3,7 +3,25 @@ import axios from 'axios'
 const BASE_URL = 'https://api.pro.coinbase.com'
 
 export function getHistoricalData (productId, range = null) {
-	return axios.get(`${BASE_URL}/products/${productId}/candles`)
+	const date = new Date()
+	date.setHours(0)
+	date.setMinutes(0)
+	date.setSeconds(0)
+	return axios.get(`${BASE_URL}/products/${productId}/candles`, {
+		params: {
+			start: date.toISOString(),
+			end: new Date().toISOString(),
+			granularity: 300
+		}
+	}).then(response => {
+		const sortFn = (a, b) => a[0] - b[0]
+		const convertEpochToCET = iso => new Date(iso * 1000)
+		const mapFn = arr => {
+			const [time, ...rest] = arr
+			return [convertEpochToCET(time), ...rest]
+		}
+		return response.data.sort(sortFn).map(mapFn)
+	})
 }
 
 export function getProducts () {
