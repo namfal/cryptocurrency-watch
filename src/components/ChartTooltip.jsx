2bb8719c0f17d613200fdc3041faf82f33c167bb
Nami Falhan <namfaldev@gmail.com>
2021-02-22 19:44:15 +0100

    using the formatted price in the chart tooltip as well

--- src/components/ChartTooltip.jsx
+++ src/components/ChartTooltip.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { formatPrice } from '../utils'
 
 const ChartTooltip = ({ active, payload }) => {
 	if (active && payload) {
 		return (
 			<div className="chart-tooltip">
 				<p className="time">{`${payload[0].payload.time}`}</p>
-				<p className="value">{`${payload[0].payload.value}`}</p>
+				<p className="value">{ formatPrice(payload[0].payload.value) }</p>
 			</div>
 		)
 	} else return null
