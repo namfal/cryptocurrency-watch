ca3d63ed52926575886fffea2454037af1b9aaa5
Nami Falhan <namfaldev@gmail.com>
2021-02-22 18:21:25 +0100

    only showing the min and the max on the y axis

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -4,6 +4,7 @@ import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'rec
 import ChartTooltip from './ChartTooltip'
 
 const Chart = ({ data }) => {
+	const yValues = data.map(item => item.value)
 	return <div className="chart-container">
 		<ResponsiveContainer width="100%" height="100%">
 			<LineChart
@@ -28,6 +29,7 @@ const Chart = ({ data }) => {
 					domain={['dataMin', 'dataMax']}
 					padding={{ bottom: 25, top: 25 }}
 					tickLine={false}
+					ticks={[Math.min(...yValues), Math.max(...yValues)]}
 				/>
 				<Tooltip content={<ChartTooltip/>} />
 				<Line type="monotone" dataKey="value" stroke="#8884d8" activeDot={{ r: 4 }} dot={false}/>
