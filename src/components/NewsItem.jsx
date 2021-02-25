85e8aef49a27fc463aba09fd8bf7c6f8be9a3ec3
Nami Falhan <namfaldev@gmail.com>
2021-02-25 19:09:25 +0100

    styled news items to make it easier to read

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,9 +4,9 @@ import { formatDate } from '../utils'
 const NewsItem = ({ newsItem }) => {
 	return <div className="news-item">
 		<a href={newsItem.url} target="_blank" rel="noreferrer">
-			<h3><span className="source-name">{newsItem.source.name} / </span>{newsItem.title}</h3>
-			<p>{newsItem.description}</p>
-			<p>{formatDate(newsItem.publishedAt, true)}</p>
+			<h3 className="news-title"><span className="source-name">{newsItem.source.name} / </span>{newsItem.title}</h3>
+			<p className="news-description">{ newsItem.description }</p>
+			<p className="published-at">{formatDate(newsItem.publishedAt, true)}</p>
 		</a>
 	</div>
 }
