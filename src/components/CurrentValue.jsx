78be0aeb67b319ba827a055762fba24423ec7767
Nami Falhan <namfaldev@gmail.com>
2021-02-26 19:51:56 +0100

    moved style of CurrentValue component to its own component, adjusted the padding of the chartcontainer

--- src/components/CurrentValue.jsx
+++ src/components/CurrentValue.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { formatPrice } from '../utils'
 
+const style = {
+	textAlign: 'right',
+	fontSize: '56px',
+	fontWeight: 600,
+	color: '#fff',
+	alignSelf: 'flex-end'
+}
+
 const CurrentValue = ({ price, currency }) => {
-	return price ? <p className="current-value">{formatPrice(price, currency)}</p> : null
+	return price ? <p className="current-value" style={style}>{formatPrice(price, currency)}</p> : null
 }
 
 export default CurrentValue
