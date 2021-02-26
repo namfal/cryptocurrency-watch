e290f013a7f316fa8be433f0f7b39fe912c917e3
Nami Falhan <namfaldev@gmail.com>
2021-02-26 17:41:59 +0100

    possing in currency to ChartTooltip component

--- src/components/ChartTooltip.jsx
+++ src/components/ChartTooltip.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { formatPrice, formatDate } from '../utils'
 
-const ChartTooltip = ({ active, payload }) => {
+const ChartTooltip = ({ active, payload, currency }) => {
 	if (active && payload) {
 		return (
 			<div className="chart-tooltip">
 				<p className="time">{ formatDate(payload[0].payload.date) }</p>
-				<p className="value">{ formatPrice(payload[0].payload.value) }</p>
+				<p className="value">{ formatPrice(payload[0].payload.value, currency) }</p>
 			</div>
 		)
 	} else return null
