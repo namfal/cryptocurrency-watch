c2f61a52fb629a2d787d069048678bfa2d37ef43
Nami Falhan <namfaldev@gmail.com>
2021-02-26 17:44:00 +0100

    added a class version of the ChartContainer component, users can now toggle between different currencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './styles/App.css'
 import React from 'react'
-import ChartContainer from './components/ChartContainer'
+// import ChartContainer from './components/ChartContainer'
+import ChartContainerClass from './components/ChartContainerClass'
 import News from './components/News'
 
 function App () {
 	return (
 		<div className="App">
 			<div className="top-container">
-				<ChartContainer />
+				<ChartContainerClass />
 			</div>
 			<News />
 		</div>
---
+++ src/components/ChartContainerClass.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+import '../styles/chart.css'
+import { getHistoricalData } from '../services/services'
+import { throttle } from 'lodash'
+import Chart from './Chart'
+import RadioButtons	from './RadioButtons'
+
+class ChartContainerClass extends React.Component {
+	constructor (props) {
+		super(props)
+		this.socket = new WebSocket('wss://ws-feed.gdax.com')
+		this.state = {
+			data: [],
+			currency: 'USD'
+		}
+		this.handleCurrencyChange = this.handleCurrencyChange.bind(this)
+	}
+
+	async componentDidMount () {
+		await this.loadHistoricalData()
+
+		this.socket.onopen = (e) => {
+			this.socket.send(JSON.stringify(this.getSubscription()))
+		}
+
+		this.socket.onerror = (error) => {
+			console.error(`[error] ${error.message}`)
+		}
+
+		this.socket.onclose = (e) => {
+			if (!e.wasClean) {
+				console.error('[close] Connection died due to a problem')
+			}
+		}
+
+		this.socket.onmessage = throttle((e) => {
+			const newData = JSON.parse(e.data)
+			if (newData.type === 'ticker') {
+				this.setState({
+					data: [...this.state.data, {
+						date: newData.time,
+						value: Number(newData.price)
+					}]
+				})
+			}
+		}, 5000)
+	}
+
+	componentWillUnmount () {
+		this.socket.close()
+	}
+
+	async loadHistoricalData () {
+		try {
+			const resp = await getHistoricalData(`BTC-${this.state.currency}`)
+			this.setState({ data: resp })
+		} catch (e) {
+			console.error(e.message)
+		}
+	}
+
+	handleCurrencyChange (value) {
+		this.setState({ currency: value, data: [] }, async () => {
+			await this.loadHistoricalData()
+			this.unsubscribe()
+			this.subscribe()
+		})
+	}
+
+	getSubscription () {
+		return {
+			type: 'subscribe',
+			channels: [
+				{
+					name: 'ticker',
+					product_ids: [`BTC-${this.state.currency}`]
+				}
+			]
+		}
+	}
+
+	unsubscribe () {
+		this.socket.send(JSON.stringify({
+			type: 'unsubscribe',
+			channels: [{ name: 'ticker' }]
+		}))
+	}
+
+	subscribe () {
+		this.socket.send(JSON.stringify(this.getSubscription()))
+	}
+
+	render () {
+		return <div className="chart-container">
+			<Chart data={this.state.data} currency={this.state.currency}/>
+			<RadioButtons
+				options={['USD', 'EUR', 'GBP']}
+				name="currency"
+				handleClick={this.handleCurrencyChange}
+				currentCurrency={this.state.currency}
+			/>
+		</div>
+	}
+}
+
+export default ChartContainerClass
