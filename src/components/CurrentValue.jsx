0fc46bdafeded32a25d4b7f4236e1ce2ce688a51
Nami Falhan <namfaldev@gmail.com>
2021-03-04 14:10:50 +0100

    moved inline styling of CurrentValue component to css and changed some padding/margin values to use em instead of px

--- src/components/CurrentValue.jsx
+++ src/components/CurrentValue.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 
-const style = {
-	textAlign: 'right',
-	fontSize: '56px',
-	fontWeight: 600,
-	color: '#fff',
-	alignSelf: 'flex-end'
-}
-
 const CurrentValue = ({ children }) => {
-	return <p className="current-value" style={style}>{children}</p>
+	return <p className="current-value">{children}</p>
 }
 
 export default CurrentValue
